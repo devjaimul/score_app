dcbb27aab40eec5ed96499234d6a15514027b475
Jaimul Islam <jaimulislam7@gmail.com>
2024-11-16 10:13:17 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:score_app/hospital.dart';
+import 'package:score_app/login.dart';
 import 'package:score_app/match_screen.dart';
 import 'firebase_options.dart';
 void main() async{
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HospitalScreen(),
+      home: Login(),
     );
   }
 }
